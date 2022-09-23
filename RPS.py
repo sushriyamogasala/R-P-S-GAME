a742dff7e65ba900d869bbccb33e5c336ea4c440
Sushriya <sushriya1009@gmail.com>
2022-09-23 21:01:48 +0530

    First commit for rps

---
+++ RPS.py
@@ -0,0 +1,38 @@
+from random import randint
+
+turns=["Rock","Paper","Scissors"]
+
+player1=turns[randint(0,2)]
+
+player2=False
+
+while player2==False:
+
+    player2=input("What is your choice Rock,Paper,Scissors ?")
+
+    if player2 == player1:
+        print("Wooo...It's a TIE!!!🤝")
+
+    elif player2 == "Rock" :
+        if player1 =="Scissors":
+            print("You won 🥳 !!!", player2 ,"smashes",player1)
+        else:
+            print("OOPS you lost 🥲 !!", player1,"covers",player2)
+
+    elif player2 == "Paper":
+        if player1 == "Rock":
+            print("You won 🥳 !!!", player2 ,"covers",player1)
+        else:
+            print("OOPS...you lost 🥲 !!", player1,"cuts",player2)
+    
+    elif player2 == "Scissors":
+        if player1 == "Paper":
+             print("You won 🥳 !!!", player2 ,"cuts",player1)
+        else:
+            print("OOPS...you lost 🥲 !!", player1,"smashes",player2)
+    
+    else :
+        print("😤 😤 😤 Check your spelling you moron.... 😒 🤬 😒!!!!!")
+
+    player2=False
+    player1=turns[randint(0,2)]
